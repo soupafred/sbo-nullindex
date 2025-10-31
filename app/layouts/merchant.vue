d70054cc3febfe26c9065162d05c454832443353
Pisatto <misapisatto@gmail.com>
2025-10-31 22:33:15 +0700

    feat(header): add header pending

--- app/layouts/merchant.vue
+++ app/layouts/merchant.vue
@@ -1,14 +1,25 @@
 <template>
   <div>
-    <UHeader class="w-full">
-      <template #title>
-        <div class="text-red">
-          <UAvatar
+    <UHeader :ui="{ root: 'h-14', container: 'max-w-none xl:px-3 px-3 sm:px-3 lg:px-3' }">
+      <template #left>
+        <div class="flex items-center gap-0 h-14 overflow-y-hidden">
+          <div class="bg-gray-50">
+            <UAvatar
+              size="lg"
+              class="rounded-none squircle shadow-md"
+              src="icons/something-logo-light.svg"
+              alt="Benjamin Canac"
+            />
+          </div>
+
+          <USeparator class="h-full ml-3" orientation="vertical" />
+          <USelectMenu
+            model-value="New Balance Cambodia"
             size="xl"
-            class="rounded-none squircle shadow-md"
-            src="icons/something-logo-light.svg"
-            alt="Benjamin Canac"
+            class="w-100 cursor-pointer"
+            :ui="{ base: 'ring-0 bg-tranparent hover:bg-gray-100 rounded-none h-full w-full px-4' }"
           />
+          <USeparator class="h-full" orientation="vertical" />
         </div>
       </template>
     </UHeader>
